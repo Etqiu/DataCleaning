f8fdd7dcb649fc60cc637ff90df661df9650bebb
Ethan <ethanqiu.sf@gmail.com>
2025-02-25 19:54:53 -0800

    trying out pdfplumber

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
-import pandas as pd
-import seaborn as sn 
+import pdfplumber 
+import pytesseract 
+
+with pdfplumber.open('data/A review of deep learning-based stereo vision techniques.pdf') as pdf:
+    for page in pdf.pages: 
+        text = page.extract_text() 
+        print(text)
+
+ 
